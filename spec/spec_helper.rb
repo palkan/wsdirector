ebd39942fe0f64ecf1c6a49243cc151cecce97b8
Grandman <root@grandman73.ru>
2017-07-04 12:42:23 +0400

    add circle.yml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,15 @@
 require 'simplecov'
+
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
 SimpleCov.start
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
 require "wsdirector"
 require 'support/echo_server'
 require 'support/fixture_helper'
 require 'pry'
 
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
