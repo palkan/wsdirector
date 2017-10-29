342d7afa676928d2404ff9fefff1e9a052f92b31
Vladimir Dementyev <dementiev.vm@gmail.com>
2017-10-29 13:19:30 +0300

    Configure RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,19 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "wsdirector"
+
+begin
+  require "pry-byebug"
+rescue LoadError # rubocop:disable Lint/HandleExceptions
+end
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+
+  config.order = :random
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
+  config.example_status_persistence_file_path = "tmp/.rspec-status"
+end
